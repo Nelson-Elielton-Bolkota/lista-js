77d9ab8de8186e20d9996b262e1ebc6ec61ce02e
Nelson <bolkotanelson@gmail.com>
2026-05-05 22:05:56 -0300

    bagueitiado

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 
 let lista_nomes = ["luis komar", "dionata", "bolsonaro"]
+let lista_maiuscula = lista_nomes.map(nome=> nome.toUpperCase())
+let lista_filtrada = lista_nomes.filter(nome => nome.length >= 5)
 
 function renderizar() {
     const lista_pagina = document.getElementById("lista")
     lista_pagina.innerHTML = ""
 
-    for (let item of lista_nomes) {
-        let elemento = document.createElement('li')
-        elemento.innerText = item
-        lista_pagina.appendChild(elemento)
-    }
+    lista_nomes.forEach(nome =>{
+        let li = document.createElement("li")
+        li.innerText = nome
+        lista_pagina.appendChild(li)
+    })
+
+    contar_nomes()
 }
+
+function renderizar2(lista_qualquer){
+    const lista_nova = document.getElementById("lista2")
+    lista_nova.innerHTML = ""
+
+    lista_qualquer.forEach(nome =>{
+        let li = document.createElement("li")
+        li.innerText = nome
+        lista_nova.appendChild(li)
+    })
+}
+
 function contar_nomes(){
+    const contagem = document.getElementById("contagem")
     let contador = 0
 
     for(let nome of lista_nomes){
-        if(nome.length > 5){
+        if(nome.length >= 5){
             contador++
         }
     }
-    return contador
+    contagem.innerText = contador
+
 }
 
-document.getElementById("add_final").addEventListener("click", () => {
+
+document.getElementById("add_final").addEventListener("click", ()=>{
     const input = document.getElementById("inputnome")
     lista_nomes.push(input.value)
     input.value = ""
@@ -42,7 +61,12 @@ document.getElementById("rv_primeiro").addEventListener("click", () => {
     lista_nomes.shift()
     renderizar()
 })
-
+document.getElementById("masculo").addEventListener("click",()=>{
+    renderizar2(lista_maiuscula)
+})
+document.getElementById("filtrado").addEventListener("click", ()=> {
+    renderizar2(lista_filtrada)
+})
 
 
 document.addEventListener("DOMContentLoaded", renderizar)
